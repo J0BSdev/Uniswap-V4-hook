15cc327f982514453570e623d029e2cde4ff849c
Lovro <lovro.posel79@gmail.com>
2026-04-03 09:30:54 +0200

    update

--- src/ExecutionGuardHook.sol
+++ src/ExecutionGuardHook.sol
@@ -1,10 +1,19 @@
-SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.30;
+//@title ExecutionGuardHook
+//@author Lovro Posel
+//@notice this hook will prevent the execution of the swap if the slippage is too high.
 
-import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
-import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
-import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
-import {BeforeSwapDelta,BeforeSwapDeltaLibrary} from "@uniswap/v4-core/contracts/types/BeforeSwapDelta.sol";
-import{Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
-import{BaseTestHooks} from "@uniswap/v4-core/contracts/libraries/BaseTestHooks.sol";
+pragma solidity ^0.8.24;
+
+import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {BeforeSwapDelta,BeforeSwapDeltaLibrary} from "@uniswap/v4-core/src/types/BeforeSwapDelta.sol";
+import{Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
+import{BaseTestHooks} from "@uniswap/v4-core/src/libraries/BaseTestHooks.sol";
+import {OnlyPoolManager} from "@uniswap/v4-core/src/libraries/OnlyPoolManager.sol";
+
+
+
+   
