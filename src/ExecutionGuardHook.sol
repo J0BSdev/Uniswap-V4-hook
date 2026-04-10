22b92900f6091bf4f68d2cf405921b5ad24864b4
Lovro <lovro.posel79@gmail.com>
2026-04-10 17:41:46 +0200

    update

--- src/ExecutionGuardHook.sol
+++ src/ExecutionGuardHook.sol
@@ -1,19 +1,13 @@
-//SPDX-License-Identifier: MIT
-
-//@title ExecutionGuardHook
-//@author Lovro Posel
-//@notice this hook will prevent the execution of the swap if the slippage is too high.
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {IHooks} from "@libraries/v4-core/src/interfaces/IHooks.sol";
-import {PoolKey} from "@libraries/v4-core/src/types/PoolKey.sol";
-import {IPoolManager} from "@libraries/v4-core/src/interfaces/IPoolManager.sol";
-import {BeforeSwapDelta,BeforeSwapDeltaLibrary} from "@libraries/v4-core/src/types/BeforeSwapDelta.sol";
-import{Hooks} from "@libraries/v4-core/src/libraries/Hooks.sol";
-import{BaseTestHooks} from "@libraries/v4-core/src/libraries/BaseTestHooks.sol";
-import {OnlyPoolManager} from "@libraries/v4-core/src/hooks/OnlyPoolManager.sol";
+/// @title ExecutionGuardHook
+/// @author Lovro Posel
+/// @notice Starter hook: extend with slippage/MEV checks inside `beforeSwap`.
+import {IHooks} from "../lib/v4-core/src/interfaces/IHooks.sol";
+import {PoolKey} from "../lib/v4-core/src/types/PoolKey.sol";
+import {IPoolManager} from "../lib/v4-core/src/interfaces/IPoolManager.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "../lib/v4-core/src/types/BeforeSwapDelta.sol";
+import {BaseTestHooks} from "../lib/v4-core/src/test/BaseTestHooks.sol";
 
 
-
-   
