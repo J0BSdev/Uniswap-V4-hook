886b7cf7e20bf9e064397aabd9a7d5c832fe6b04
Lovro <lovro.posel79@gmail.com>
2026-04-12 13:03:39 +0200

    update

--- src/GasPriceFeesHook.sol
+++ src/GasPriceFeesHook.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 /// @title GasPriceFeesHook
 /// @author Lovro Posel
 /// @notice Hook to check the gas price and fees of the transaction
-import {BaseHooks} from "../lib/v4-core/src/hooks/BaseHooks.sol";
+import {BaseHooks} from "../lib/v4-periphery/src/hooks/BaseHooks.sol";
 import {IHooks} from "../lib/v4-core/src/interfaces/IHooks.sol";
 import {IPoolManager} from "../lib/v4-core/src/interfaces/IPoolManager.sol";
 import {PoolKey} from "../lib/v4-core/src/types/PoolKey.sol";
