f429cc65823a627b14adc42aeee6b7052f02698d
Lovro <lovro.posel79@gmail.com>
2026-04-15 22:13:24 +0200

    update

--- src/GasPriceFeesHook.sol
+++ src/GasPriceFeesHook.sol
@@ -11,7 +11,7 @@ import {BalanceDelta} from "../lib/v4-core/src/types/BalanceDelta.sol";
 import {Hooks} from "../lib/v4-core/src/libraries/Hooks.sol";
 import {LPFeeLibrary} from "../lib/v4-core/src/libraries/LPFeeLibrary.sol";
 import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "../lib/v4-core/src/types/BeforeSwapDelta.sol";
-import {SwapParams} from "../lib/v4-core/src/types/PoolOperation.sol";
+import {SwapParams} from "../lib/v4-core/src/test/PoolSwapTest.sol";
 
 contract GasPriceFeesHook is BaseHook {
     using LPFeeLibrary for uint24;
