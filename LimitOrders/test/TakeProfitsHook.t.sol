088dd1c203a3056d9a5f6f12970843ef2277e25a
Lovro <lovro.posel79@gmail.com>
2026-04-26 13:08:35 +0200

    update

--- LimitOrders/test/TakeProfitsHook.t.sol
+++ LimitOrders/test/TakeProfitsHook.t.sol
@@ -260,7 +260,7 @@ contract TakeProfitsHookTest is Test, Deployers, ERC1155Holder{
         assertEq(pendingInputTokensForOrderAtTick60, 0);
 
         uint256 pendingInputTokensForOrderAtTick120 = hook.pendingOrders(key.toId(), 120, true);
-        assertEq(pendingInputTokensForOrderAtTick120, amount * 2);
+        assertEq(pendingInputTokensForOrderAtTick120, 0);
 
 
 
