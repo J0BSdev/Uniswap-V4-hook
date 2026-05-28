5cceeae0bf22b6b50a013a7c47e80bbd5ec6aa3b
Lovro <lovro.posel79@gmail.com>
2026-05-28 21:50:22 +0200

    update

--- src/LPFees/DynamicLPFeesHook.sol
+++ src/LPFees/DynamicLPFeesHook.sol
@@ -5,17 +5,20 @@ pragma solidity ^0.8.24;
 /// @author Lovro Posel
 /// @notice Uniswap v4 hook that charges a dynamic fee based on the liquidity of the pool.
 
-import {BaseHook} from "../lib/v4-hooks-public/src/base/BaseHook.sol";
-import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
-import {IHooks} from "@uniswap/v4-core/src/interfaces/IHooks.sol";
-import {Hooks} from "@uniswap/v4-core/src/libraries/Hooks.sol";
-import {LPFeeLibrary} from "../lib/LPFeeLibrary.sol";
-import {StatsLibrary} from "../lib/StatsLibrary.sol";
-import {SwapParams} from "@uniswap/v4-core/src/libraries/SwapParams.sol";
-import {PoolKey,PoolId} from "@uniswap/v4-core/src/libraries/PoolKey.sol";
-import {BeforeSwapDelta} from "@uniswap/v4-core/src/libraries/BeforeSwapDelta.sol";
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {SwapParams} from "v4-core/src/types/PoolOperation.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+
 
 
 contract DynamicLPFeesHook is BaseHook {
-        constructor(IPoolManager poolManager) BaseHook(poolManager) {}
+        constructor(IPoolManager poolManager) BaseHook(poolManager) {
+
+                using
+        }
 } 
