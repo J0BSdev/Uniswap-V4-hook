9bc0c9cc098b0d48818bd134058dca607c7d145b
Lovro <lovro.posel79@gmail.com>
2026-06-02 22:13:11 +0200

    update

--- src/LPFees/RiskModelLib.sol
+++ src/LPFees/RiskModelLib.sol
@@ -7,21 +7,17 @@ import {Types} from "./Types.sol";
 /// @notice Scores a swap from pool state. Uses trade size vs liquidity and price drift.
 library RiskModelLib {
     uint256 internal constant MAX_BPS = 10_000;
-    
 
     function computeRiskScore(Types.RiskInputs memory i) internal pure returns (Types.RiskScore memory score) {
         score.sizeRatioBps = _sizeRatioBps(i.tradeSize, i.liquidity);
         score.priceDeviationBps = _priceDeviationBps(i.sqrtPriceX96, i.referenceSqrtPriceX96);
-        
     }
 
     function _sizeRatioBps(uint256 tradeSize, uint128 liquidity) private pure returns (uint256) {
         if (liquidity == 0) return MAX_BPS;
-       
     }
 
     function _priceDeviationBps(uint160 sqrtPriceX96, uint160 refSqrtPriceX96) private pure returns (uint256) {
         if (refSqrtPriceX96 == 0) return 0;
-       
     }
 }
