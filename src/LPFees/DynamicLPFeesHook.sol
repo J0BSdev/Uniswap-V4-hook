dd97d6dc31b9b0cefae4ef88bcb95a54d6d1b17f
Lovro <lovro.posel79@gmail.com>
2026-06-05 22:39:24 +0200

    update

--- src/LPFees/DynamicLPFeesHook.sol
+++ src/LPFees/DynamicLPFeesHook.sol
@@ -21,8 +21,6 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 contract DynamicLPFeesHook is BaseHook {
     using LPFeeLibrary for uint24;
     using StateLibrary for IPoolManager;
-    using AggregatorV3Interface for PriceDataFeed;
-
 
     error MustUseDynamicFees();
 
@@ -38,19 +36,20 @@ contract DynamicLPFeesHook is BaseHook {
 AggregatorV3Interface internal PriceDataFeed;
     
 
-    constructor(IPoolManager _manager, address _dataFeed) BaseHook(_manager) {
-        PriceDataFeed = AggregatorV3Interface(_dataFeed);
+    constructor(IPoolManager _manager) BaseHook(_manager) {
+        PriceDataFeed = AggregatorV3Interface(0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1);
     }
 
 
-
-    function getLatestPrice()
-    internal
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) {
-    (,int256 answer,,,) = PriceDataFeed.latestRoundData();
-    return (roundId, answer, startedAt, updatedAt, answeredInRound);
+function latestRoundData()
+  public
+  view
+  returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+{
+  return PriceDataFeed.latestRoundData();
 }
+
+
 
 
     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
@@ -81,24 +80,28 @@ AggregatorV3Interface internal PriceDataFeed;
         internal
         override
         returns (bytes4)
-
+    {
         PoolId poolId = key.toId();
-        uint24  = getHistoricalFee(poolId);
+
+        (, int256 answer,,,) = PriceDataFeed.latestRoundData();
+
         return BaseHook.afterInitialize.selector;
     }
 
-    function _beforeSwap (
+    function _beforeSwap(
         address sender,
         PoolKey calldata key,
         SwapParams calldata params,
         bytes calldata hookData
-    ) internal override returns (bytes4, BeforeSwapDelta, uint24 historicalFee) {
+    ) internal override returns (bytes4, BeforeSwapDelta, uint24 ) {
+PoolId poolId = key.toId();
+
         // Use time-weighted average price from trusted oracle
         // This cannot be manipulated within a single transaction
-    int256 latestPrice = getLatestPrice();
-
-        
-        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA,| LPFeeLibrary.OVERRIDE_FEE_FLAG);
+    (, int256 answer,,,) = PriceDataFeed.latestRoundData();
+        return (BaseHook.beforeSwap.selector,
+         BeforeSwapDeltaLibrary.ZERO_DELTA, 
+         feePips | LPFeeLibrary.OVERRIDE_FEE_FLAG);
     }
     
 }
